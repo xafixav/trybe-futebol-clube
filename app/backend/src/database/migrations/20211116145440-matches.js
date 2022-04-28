7651950ba29482bfd48d417b00cdbb3bb90a4d89
Xafixav <xafiworld@gmail.com>
2022-04-28 18:18:06 -0300

    :white_check_mark: migrations and seeders done

---
+++ app/backend/src/database/migrations/20211116145440-matches.js
@@ -0,0 +1,42 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id'          
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id'          
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+      },
+      in_progress: {
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
