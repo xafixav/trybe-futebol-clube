fb68b2149ce9d246ea236d7631c0024003f8ec37
xafixav <xafiworld@gmail.com>
2022-05-04 17:54:56 -0300

    :sparkles: add getUser Service and refactor few methods

--- app/backend/src/service/Login.ts
+++ app/backend/src/service/Login.ts
@@ -1,20 +1,24 @@
 import * as bcryptjs from 'bcryptjs';
+import { StatusCodes } from 'http-status-codes';
 import Users from '../database/models/Users';
 import ILoginResponse from '../interfaces/ILoginResponse';
 import ILogin from '../interfaces/ILogin';
 import JwtService from './Jwt';
-import ErrorExtension from '../ErrorExtension';
+import ErrorExtension from '../utility/ErrorExtension';
 
 export default class LoginService {
   jwt: JwtService;
 
+  private user;
+
   constructor() {
     this.jwt = new JwtService();
+    this.user = Users;
   }
 
   public findUser = async (data: ILogin): Promise<ILoginResponse> => {
     const { email, password } = data;
-    const userPayload: any = await Users.findOne({ where: { email } });
+    const userPayload = await Users.findOne({ where: { email } });
 
     if (!userPayload) {
       throw new ErrorExtension({ status: 400, message: 'Incorrect email or password' });
@@ -38,8 +42,23 @@ export default class LoginService {
     const validUser = bcryptjs.compareSync(password, payload.password);
 
     if (!validUser) {
-      throw new ErrorExtension({ status: 400, message: 'Incorrect email or password' });
+      throw new ErrorExtension({ status: StatusCodes.BAD_REQUEST,
+        message: 'Incorrect email or password' });
     }
     return validUser;
+  };
+
+  public getUserRole = async (token: string) => {
+    try {
+      const { email }: any = this.jwt.validateToken(token);
+
+      const User = await this.user.findOne({ where: { email } });
+
+      if (User) {
+        return User.role;
+      }
+    } catch (e: any) {
+      throw new ErrorExtension({ status: StatusCodes.BAD_REQUEST, message: e.message });
+    }
   };
 }
