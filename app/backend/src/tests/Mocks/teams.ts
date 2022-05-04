f6fd8f1e48e0887c221f37b72d429bb0968e8293
xafixav <xafiworld@gmail.com>
2022-05-04 20:55:02 -0300

    :clown_face: add Teams Mocks for tests

---
+++ app/backend/src/tests/Mocks/teams.ts
@@ -0,0 +1,19 @@
+export const TeamsMocked = [
+	{
+		"id": 1,
+		"teamName": "Avaí/Kindermann"
+	},
+	{
+		"id": 2,
+		"teamName": "Bahia"
+	},
+	{
+		"id": 3,
+		"teamName": "Botafogo"
+	},
+];
+
+export const TeamId2Mock = 	{
+  "id": 2,
+  "teamName": "Bahia"
+};
