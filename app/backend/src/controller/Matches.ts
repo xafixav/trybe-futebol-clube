bf19c69f11d935ba6db9b244dba6b1d528505801
xafixav <xafiworld@gmail.com>
2022-05-11 20:46:14 -0300

    :sparkles: add create, update and End Matches into MatchesController

--- app/backend/src/controller/Matches.ts
+++ app/backend/src/controller/Matches.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import IUpdateMatch from '../interfaces/IUpdateMatch';
 import MatchesService from '../service/Matches';
 
 export default class MatchesController {
@@ -18,6 +19,43 @@ export default class MatchesController {
       }
       const allMatches = await this.MatchesService.findAll();
       return res.status(StatusCodes.OK).json(allMatches);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  public createMatch = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress } = req.body;
+      const matchesFiltered = await this.MatchesService
+        .createMatch({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress });
+      return res.status(StatusCodes.CREATED).json(matchesFiltered);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  public endMatch = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const matchEnded = await this.MatchesService.endMatch(+id);
+      return res.status(StatusCodes.OK).json(matchEnded);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  public updateMatch = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { homeTeamGoals, awayTeamGoals } = req.body;
+      const { id } = req.params;
+      const updateRequest: IUpdateMatch = {
+        homeTeamGoals,
+        awayTeamGoals,
+        id: +id,
+      };
+      const matchUpdated = await this.MatchesService.updateMatch(updateRequest);
+      return res.status(StatusCodes.OK).json(matchUpdated);
     } catch (e) {
       next(e);
     }
