66bbf19ac61e8bacbe6c84fa18ad57fde612cb08
xafixav <xafiworld@gmail.com>
2022-05-11 20:47:50 -0300

    :white_check_mark: add routes for post /matches, patch /matches/:id/finish and patch /matches/:id/

--- app/backend/src/Routes/Matches.ts
+++ app/backend/src/Routes/Matches.ts
@@ -1,14 +1,32 @@
 import * as express from 'express';
 import Matches from '../controller/Matches';
 
-const { findAll } = new Matches();
+const MatchController = new Matches();
 
 const router = express.Router();
 
 router
   .route('/matches')
   .get(
-    findAll,
+    MatchController.findAll,
+  );
+
+router
+  .route('/matches')
+  .post(
+    MatchController.createMatch,
+  );
+
+router
+  .route('/matches/:id/finish')
+  .patch(
+    MatchController.endMatch,
+  );
+
+router
+  .route('/matches/:id/')
+  .patch(
+    MatchController.updateMatch,
   );
 
 export default router;
